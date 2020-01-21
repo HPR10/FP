a40118503a232d214286d7971854cdd5f3debc8c
Hugo Pinheiro Raimundo <hugo_hp10@hotmail.com>
2020-01-20 21:25:43 -0300

    Leitura de caracteristicas de uma string

---
+++ ex004.py
@@ -0,0 +1,9 @@
+n = input('Digite algo: ')
+print('Qual o tipo primitivo: ', type(n))
+print('É numérico: ', n.isnumeric())
+print('É alfabético: ', n.isalpha())
+print('É alfanumérico: ', n.isalnum())
+print('Tem letras maiuscula: ',n.isupper())
+
+
+
