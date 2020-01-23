2800233770a25921f2b87e6716ae4c89142f6913
Hugo Pinheiro Raimundo <hugo_hp10@hotmail.com>
2020-01-22 22:06:59 -0300

    Programa que leia duas notas e calcula a média

---
+++ ex007.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule a sua média.
+n1 = int(input('Digite a sua primeira nota: '))
+n2 = int(input('Digite a sua segunda nota: '))
+media = (n1 + n2) / 2
+print(media)
