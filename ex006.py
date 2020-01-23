c7268587b2725e4a27a25a45adc57725e083d491
Hugo Pinheiro Raimundo <hugo_hp10@hotmail.com>
2020-01-22 21:58:28 -0300

    Algoritmo que mostra o dobro, triplo e a raiz quadrada

---
+++ ex006.py
@@ -0,0 +1,6 @@
+# Cria uma algoritmo que mostre o seu dobro, triplo e a raiz quadrada.
+n = int(input('Digite um número: '))
+dobro = n * 2 
+triplo = n * 3 
+raiz = n**(1/2)
+print('Pelo número digitado o dobro é {} e o triplo é {} e sua raiz quadrada é {:.0f}'.format(dobro, triplo, raiz))
