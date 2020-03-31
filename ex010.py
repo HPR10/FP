81405d7890e579ae3addc4daf7c6f0b0b5043ac2
Hugo Pinheiro <hugopinheiro@MacBook-Pro-de-Hugo.local>
2020-03-31 00:15:43 -0300

    Calculando valores na carteira em dolar

---
+++ ex010.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia quanto dinheiro uma pessao tem na carteira e mostre quantos dólares ela pode comprar.
+#Dolar = 4,10 
+v = float(input('Quanto dinheiro você tem na carteira: '))
+d = 4.10
+t = v * d
+print('O valor em dolar que você pode comprar é: {:.2f}'.format(t))
+
