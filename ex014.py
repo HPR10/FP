5f8b0ab4e4793faa53301773fdfd14a1fa43f9f4
Hugo Pinheiro Raimundo <hugo_hp10@hotmail.com>
2020-04-28 19:57:36 -0300

    Programa que converte Celsius para fahrenheit

---
+++ ex014.py
@@ -0,0 +1,5 @@
+#Programa que converte Celsius para fahrenheit
+c = float(input('Informe a temperatura em °C: '))
+f = (c * 9/5) + 32 
+print('A temperatura de {}°C corresponde a {}F'.format(c, f))
+
